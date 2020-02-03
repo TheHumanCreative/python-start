3d2ce6275588718041087a7ce373fa41ac98fc6f
Miles Lacek <mileslacek@gmail.com>
2020-02-03 12:06:55 -0600

    started python course on Codecademy

---
+++ python codecademy.py
@@ -0,0 +1,11 @@
+#comments in python are started with a #
+#testing out the print statements
+print ("Hello World!")
+print ("My Name is Miles Lacek")
+print ("I am 30 years old and I love to bike")
+# expected results in terminal is the print statements to read as they are above. CHECK 
+# python 2 and python 3 are similar but syntax for python 3 uses parantheses
+
+print ("Miles" + "Lacek") # joins the two strings together without space
+print ("Miles Lacek") # prints the string just as is with a space 
+
