b570b8d97719972d52955ae98dd7549bc3b9e83e
Miles Lacek <mileslacek@gmail.com>
2020-02-12 17:19:47 -0600

    working on python 2 functions with if / else statements.

--- python codecademy.py
+++ python codecademy.py
@@ -132,3 +132,73 @@ A frog jumps in:
 Plop!"""
 
 print (haiku)
+
+#BOOLEANS 
+
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+
+# created a boolean relationship for if Maria's age is 12 and it is not so it is False.
+age_is_12 = False
+print (age_is_12)
+
+# created a boolean relationship for if Maria's name is Maria and it is true as a boolean.
+name_is_maria = True
+print (name_is_maria)
+
+# VALUE ERROR
+float_1 = 0.25
+float_2 = 40.0
+
+product = float_1 * float_2
+print (product)
+
+#printing out the product as a string with the float converted to an integer.
+big_string = "The product was " + str(int(product))
+print (big_string)
+
+number1 = "100"
+number2 = "10"
+
+string_addition = number1 + number2
+print (string_addition)
+#string_addition now has a value of "10010"
+
+int_addition = int(number1) + int(number2)
+print (int_addition)
+#int_addition has a value of 110
+
+string_num = "7.5"
+print (string_num)
+print (float(string_num))
+
+
+def cube(number):
+  return number * number * number
+
+
+cube(9)
+print (cube(9))
+#gives result of the cube function of 9 * 9 * 9 = 729
+cube(3)
+print (cube(3))
+#gives result of the cube function of 3 * 3 * 3 = 27
+
+
+def by_three(number):
+  if number % 3 == 0:
+    return cube(number)
+  else:
+    return False
+
+#Don’t forget that if and else statements need a : at the end of #that line!
+
+
+#testing the function to return a True value as it should due to #the logic that 90 is divisible by 3 thus then the function calls #the cube function to run the cube of 90 = 729,000
+by_three(90)
+print (by_three(90))
+
+#testing the function to return a False value, as it should and #does.
+by_three(10)
+print (by_three(10))
